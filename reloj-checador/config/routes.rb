4760a837b61359332cf175febe797e436ff6120d
julio cabrera <juliocabrera820@gmail.com>
2021-07-03 20:15:22 -0500

    add routes for admin and attendance

--- reloj-checador/config/routes.rb
+++ reloj-checador/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  devise_scope :user do
+    unauthenticated :user do
+      root 'attendance#check', as: :unauthenticated_admin
+    end
+  end
+
+  get 'dashboard', to: 'admin#dashboard'
 end
