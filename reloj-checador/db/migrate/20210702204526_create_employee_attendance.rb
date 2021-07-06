fdc9df230c1ed740f6e587971ae78db94f3899d3
Jairo <olan.mayo05@gmail.com>
2021-07-06 12:00:34 -0500

    update type field

--- reloj-checador/db/migrate/20210702204526_create_employee_attendance.rb
+++ reloj-checador/db/migrate/20210702204526_create_employee_attendance.rb
@@ -1,7 +1,7 @@
 class CreateEmployeeAttendance < ActiveRecord::Migration[6.1]
   def change
     create_table :employee_attendance do |t|
-      t.integer :type
+      t.integer :type, default: 1
 
       t.timestamps
     end
