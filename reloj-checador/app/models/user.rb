6c82d6c4ed2043e35f714569a0d091ce1bc9cbf8
julio cabrera <juliocabrera820@gmail.com>
2021-07-09 22:09:29 -0500

    disable user validations

--- reloj-checador/app/models/user.rb
+++ reloj-checador/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
   belongs_to :company
   has_many :employee_attendance
   enum status: %i[active inactive]
