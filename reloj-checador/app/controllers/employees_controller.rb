22d655eec6f336f4d1c108a00f464f49409fbc03
julio cabrera <juliocabrera820@gmail.com>
2021-07-11 14:27:39 -0500

    implement pagination

--- reloj-checador/app/controllers/employees_controller.rb
+++ reloj-checador/app/controllers/employees_controller.rb
@@ -2,7 +2,7 @@ class EmployeesController < ApplicationController
   before_action :set_employee, only: %i[show edit update destroy]
 
   def index
-    @employees = User.all.where(role: 'employee')
+    @pagy, @employees = pagy(User.all.where(role: 'employee'), items: 8)
   end
 
   def new
