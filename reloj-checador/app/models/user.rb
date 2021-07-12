cd7e791b31064caaa24dcf6cf2f128c43306a1fd
julio cabrera <juliocabrera820@gmail.com>
2021-07-12 09:44:14 -0500

    ignore validations

--- reloj-checador/app/models/user.rb
+++ reloj-checador/app/models/user.rb
@@ -3,8 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
+
   belongs_to :company
   has_many :employee_attendance
+
+  # validates_presence_of :name, :email, :position, :private_number, :employee_number, :company_id
+
   enum status: %i[active inactive]
   enum role: %i[employee admin]
 end
