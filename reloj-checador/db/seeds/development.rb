f75526835ec08ec8eb7862fe3ee0b55523f46b90
Jairo <olan.mayo05@gmail.com>
2021-07-14 10:14:06 -0500

    update development seed

--- reloj-checador/db/seeds/development.rb
+++ reloj-checador/db/seeds/development.rb
@@ -1,1 +1,3 @@
-User.create({ name: 'george', email: 'geo@gmail.com', password: '123456' })
+Company.create([{ name: 'bcoders', address: 'c 34 #850 x 90 y 92' }, { name: 'wise', address: 'c 41 #652 x 70 y 72' },
+                { name: 'line', address: 'c 28 #356 x 21 y 23' }])
+User.create name: 'george', email: 'geo@gmail.com', password: '123456', company_id: 1
