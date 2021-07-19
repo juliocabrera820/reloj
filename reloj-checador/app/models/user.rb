73f20b56f572b63df7ba084b68edb833d22a28e0
julio cabrera <juliocabrera820@gmail.com>
2021-07-19 17:01:13 -0500

    remove unneeded code

--- reloj-checador/app/models/user.rb
+++ reloj-checador/app/models/user.rb
@@ -7,8 +7,6 @@ class User < ApplicationRecord
   belongs_to :company
   has_many :employee_attendance
 
-  # validates_presence_of :name, :email, :position, :private_number, :employee_number, :company_id
-
   enum status: %i[active inactive]
   enum role: %i[employee admin]
 end
