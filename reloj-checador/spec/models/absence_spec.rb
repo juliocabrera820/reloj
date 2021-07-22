d56fd5bf34d3cb430116ef967e4896cc80b0405c
Jairo <olan.mayo05@gmail.com>
2021-07-22 11:17:11 -0500

    update matchers for test and validations
    Co-authored-by: Julio Cabrera <juliocabrera820@gmail.com>

--- reloj-checador/spec/models/absence_spec.rb
+++ reloj-checador/spec/models/absence_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Attendance, type: :model do
+RSpec.describe Absence, type: :model do
   describe 'associations' do
     it { should belong_to(:user) }
   end
