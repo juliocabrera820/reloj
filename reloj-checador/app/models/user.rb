a7c4ad47ed6d4ca057731159155af361274410a4
Jairo <olan.mayo05@gmail.com>
2021-07-22 10:20:46 -0500

    update relation between models

--- reloj-checador/app/models/user.rb
+++ reloj-checador/app/models/user.rb
@@ -5,8 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable
 
   belongs_to :company
-  has_many :employee_attendance
-
+  has_many :attendances
+  has_many :absences
   enum status: %i[active inactive]
   enum role: %i[employee admin]
+
+  validates :company_id, presence: true
+  validates :email, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 8 }
+  validates :private_number, :employee_number, uniqueness: { case_sensitive: false }
 end
