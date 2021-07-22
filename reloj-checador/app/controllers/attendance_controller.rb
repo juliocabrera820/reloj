6a2fad69e4a49477344b044b42f7ef4f7909449e
Jairo <olan.mayo05@gmail.com>
2021-07-22 11:19:39 -0500

    remove files from app folder

--- reloj-checador/app/controllers/attendance_controller.rb
+++
@@ -1,3 +0,0 @@
-class AttendanceController < ApplicationController
-  def check; end
-end
--- reloj-checador/app/models/employee_attendance.rb
+++
@@ -1,5 +0,0 @@
-class EmployeeAttendance < ApplicationRecord
-  belongs_to :user
-  belongs_to :attendance
-  enum type: %i[check_in check_out]
-end
