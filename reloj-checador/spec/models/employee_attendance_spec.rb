1a0f2b201d49812524b7d46189c514173d3b399a
Jairo <olan.mayo05@gmail.com>
2021-07-22 11:23:34 -0500

    remove employee_attendances spec

--- reloj-checador/spec/models/employee_attendance_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe EmployeeAttendance, type: :model do
-  describe 'associations' do
-    it { should belong_to(:user) }
-    it { should belong_to(:attendance) }
-  end
-end
