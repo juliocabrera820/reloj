743e88dc50bbc9d2335d70184908fbdba46c2eb0
Jairo <olan.mayo05@gmail.com>
2021-07-22 11:21:05 -0500

    remove migrations

--- reloj-checador/db/migrate/20210702204526_create_employee_attendance.rb
+++
@@ -1,9 +0,0 @@
-class CreateEmployeeAttendance < ActiveRecord::Migration[6.1]
-  def change
-    create_table :employee_attendance do |t|
-      t.integer :type, default: 1
-
-      t.timestamps
-    end
-  end
-end
