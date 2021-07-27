8550a6cf9caa22abf40b1ae58c202170805ff4ea
julio cabrera <juliocabrera820@gmail.com>
2021-07-27 14:34:10 -0500

    add validation tests

--- reloj-checador/spec/models/user_spec.rb
+++ reloj-checador/spec/models/user_spec.rb
@@ -10,8 +10,11 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:company_id) }
     subject { FactoryBot.create :user }
-    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
-    it { should validate_length_of(:password).is_at_least(8) }
+    attributes = %i[name email private_number employee_number position company_id]
+    attributes.each do |attribute|
+      it { should validate_presence_of(attribute) }
+    end
+    it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_uniqueness_of(:private_number).case_insensitive }
     it { should validate_uniqueness_of(:employee_number).case_insensitive }
   end
