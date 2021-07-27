e50cf6e9ce6978d04befbbd3280543d8cf7279a4
Jairo <olan.mayo05@gmail.com>
2021-07-27 13:31:38 -0500

    add action for views

--- reloj-checador/app/controllers/reports_controller.rb
+++ reloj-checador/app/controllers/reports_controller.rb
@@ -1,3 +1,9 @@
 class ReportsController < ApplicationController
     def index; end
+    
+    def daily_attendance; end
+    def monthly_attendance; end
+    def monthly_absence; end
+    def average_time; end
+
 end
