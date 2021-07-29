dfad0cb7f25aedde6c45e08f6eed6c0b405d3cb9
julio cabrera <juliocabrera820@gmail.com>
2021-07-29 00:47:43 -0500

    generate reports

--- reloj-checador/app/controllers/reports_controller.rb
+++ reloj-checador/app/controllers/reports_controller.rb
@@ -1,7 +1,62 @@
 class ReportsController < ApplicationController
+  include ReportHelper
+
+  before_action :set_companies
+
   def index; end
+
   def daily_attendance; end
-  def monthly_attendance; end
+
+  def daily_attendance_pdf
+    @users = AttendancesQuery.attendance_by_day_and_company(params[:id])
+    @company = params[:company]
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "company_daily_attendance_#{@company}", template: 'reports/attendance_day.html.erb'
+      end
+    end
+  end
+
   def monthly_absence; end
-  def average_time; end
+
+  def monthly_absence_pdf
+    @month = month_name(params[:id])
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "monthly_absence_#{@month}", template: 'reports/month_absence.html.erb'
+      end
+    end
+  end
+
+  def average_time_in; end
+
+  def average_time_in_pdf
+    @month = month_name(params[:id])
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "avg_time_in_employee_#{@month}", template: 'reports/avg_time_in.html.erb'
+      end
+    end
+  end
+
+  def average_time_out; end
+
+  def average_time_out_pdf
+    @month = month_name(params[:id])
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "avg_time_out_employee_#{@month}", template: 'reports/avg_time_out.html.erb'
+      end
+    end
+  end
+
+  private
+
+  def set_companies
+    @companies = Company.all
+  end
 end
